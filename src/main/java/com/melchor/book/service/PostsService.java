940998f30ec8da488d63b3693cc09b6ba50ee2d6
sam <chtlstjd01@gmail.com>
2020-03-06 00:06:13 +0900

    posts 조회 기능 테스트

---
+++ src/main/java/com/melchor/book/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.melchor.book.service;
+
+import com.melchor.book.domain.posts.PostsRepository;
+import com.melchor.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/melchor/book/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.melchor.book.web;
+
+import com.melchor.book.domain.posts.Posts;
+import com.melchor.book.service.PostsService;
+import com.melchor.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
